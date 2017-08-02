593f18f1c4ec501b15dff013ce2499b542f375db
wheejuni <wheejuni@gmail.com>
2017-08-02 15:36:06 +0900

    user 입력 간소

--- src/main/java/com/wheejuni/spring/UserController.java
+++ src/main/java/com/wheejuni/spring/UserController.java
@@ -15,15 +15,8 @@ public class UserController {
 	ArrayList<User> userlist = new ArrayList<>();
 
 	@PostMapping("/users")
-	public ModelAndView create(String userId, String password, String name, String email) {
+	public ModelAndView create(User user) {
 		
-
-		User user = new User();
-		user.setId(userId);
-		user.setEmail(email);
-		user.setPassword(password);
-		user.setUsername(name);
-
 		userlist.add(user);
 		ModelAndView userview = new ModelAndView("users/list");
 		userview.addObject("users", userlist);
