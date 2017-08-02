7eb6b47fc46aa38903295fac52aa955ad7f49f50
wheejuni <wheejuni@gmail.com>
2017-08-02 17:01:13 +0900

    모르겠

---
+++ src/main/java/com/wheejuni/spring/QuestionsController.java
@@ -0,0 +1,42 @@
+package com.wheejuni.spring;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wheejuni.spring.domain.Question;
+
+@Controller
+public class QuestionsController {
+	ArrayList<Question>questions = new ArrayList<>();
+	
+	@PostMapping("/qna")
+	public ModelAndView create(Question question) {
+		
+		questions.add(question);
+		return new ModelAndView("redirect:/index.html");
+	}
+	
+	@GetMapping("/qna")
+	public ModelAndView list() {
+		
+		ModelAndView mav = new ModelAndView("qna/list");
+		mav.addObject("questions", questions);
+		return mav;
+	}
+	
+	@GetMapping("/qna/{index}")
+	
+	public ModelAndView show(@PathVariable int index) {
+		
+		Question question = questions.get(index);
+		ModelAndView showDetailQuestion = new ModelAndView("qna/show");
+		showDetailQuestion.addObject("qnainfo", question);
+		return showDetailQuestion;
+	}
+
+}
---
+++ src/main/java/com/wheejuni/spring/domain/Question.java
@@ -0,0 +1,31 @@
+package com.wheejuni.spring.domain;
+
+public class Question {
+	
+	String content, author, title;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
