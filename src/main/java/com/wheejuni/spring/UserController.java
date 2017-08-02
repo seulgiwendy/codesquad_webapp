0dcb918c860f4f98b495b38b756da36a1f3e9246
wheejuni <wheejuni@gmail.com>
2017-08-02 15:46:31 +0900

    GET/POST 메소드 분

--- src/main/java/com/wheejuni/spring/UserController.java
+++ src/main/java/com/wheejuni/spring/UserController.java
@@ -3,6 +3,7 @@ package com.wheejuni.spring;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,9 +19,15 @@ public class UserController {
 	public ModelAndView create(User user) {
 		
 		users.add(user);
+		
+		System.out.println(users.size());
+		return new ModelAndView("redirect:/users");
+	}
+	
+	@GetMapping("/users")
+	public ModelAndView list() {
 		ModelAndView userview = new ModelAndView("users/list");
 		userview.addObject("users", users);
-		System.out.println(users.size());
 		return userview;
 	}
 
