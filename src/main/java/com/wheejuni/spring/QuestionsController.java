ef0cd1d173bb3d9fc28acaa176c9903c78bfa960
wheejuni <wheejuni@gmail.com>
2017-08-02 22:29:46 +0900

    -답변 등록기능 구

--- src/main/java/com/wheejuni/spring/QuestionsController.java
+++ src/main/java/com/wheejuni/spring/QuestionsController.java
@@ -14,7 +14,7 @@ import com.wheejuni.spring.domain.Question;
 @Controller
 public class QuestionsController {
 	static ArrayList<Question>questions = new ArrayList<>();
-	
+	ArrayList <Answer> answers = new ArrayList<>();
 	@PostMapping("/qna")
 	public ModelAndView create(Question question) {
 		
@@ -39,18 +39,24 @@ public class QuestionsController {
 		Question question = questions.get(index);
 		ModelAndView showDetailQuestion = new ModelAndView("qna/show");
 		showDetailQuestion.addObject("qnainfo", question);
+		showDetailQuestion.addObject("answerinfo", question.getAnswers());
+		//System.out.println(question.getAnswers().get(0).getContent());
+		showDetailQuestion.addObject("answercount", question.getAnswers().size());
+		
 		return showDetailQuestion;
 	}
 	
 	@PostMapping("/qna/{index}")
 	public ModelAndView addAnswer(@PathVariable int index, Answer answer) {
-		ArrayList <Answer> answers = new ArrayList<>();
+		
 		answer.setTime();
 		answers.add(answer);
 		Question temp = questions.get(index);
-		System.out.println(temp.toString());
+		System.out.println(answer.getContent());
+		temp.setAnswers(answers);
 		questions.remove(index);
 		questions.add(index, temp);
+		System.out.println(questions.get(index).getAnswers().get(0).getContent());
 		return new ModelAndView("redirect:/qna");
 	}
 
