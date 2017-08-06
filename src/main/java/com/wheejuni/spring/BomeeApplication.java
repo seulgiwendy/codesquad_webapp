c7940ba1fac68a41ef0bd9d8a8c439878e2e4144
wheejuni <wheejuni@gmail.com>
2017-08-06 17:19:20 +0900

    h2 database 디펜던시 수정

--- src/main/java/com/wheejuni/spring/BomeeApplication.java
+++ src/main/java/com/wheejuni/spring/BomeeApplication.java
@@ -5,12 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.wheejuni.spring.domain.UserRepository;
+
 
 @SpringBootApplication
 public class BomeeApplication{
-	
-	
+	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(BomeeApplication.class, args);
 	}
--- src/main/java/com/wheejuni/spring/UserController.java
+++ src/main/java/com/wheejuni/spring/UserController.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.wheejuni.spring.domain.*;
 import com.wheejuni.spring.domain.User;
 import com.wheejuni.spring.domain.UserRepository;
+
+
 
 
 
