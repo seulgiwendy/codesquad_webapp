b47f7abfe68672c9f0d5c9d3856fec91adba44c8
wheejuni <wheejuni@gmail.com>
2017-08-07 18:04:36 +0900

    test fail code 수정

--- src/test/java/com/wheejuni/spring/BomeeApplicationTests.java
+++ src/test/java/com/wheejuni/spring/BomeeApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest
 public class BomeeApplicationTests {
 
